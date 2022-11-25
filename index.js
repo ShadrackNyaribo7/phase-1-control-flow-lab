4ad085276c5bf55883ed0014cef4b2c21b14cb28
shadracknyaribo <stilespulsar77@gmail.com>
2022-11-25 10:49:57 -0800

    added

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 function scuberGreetingForFeet(){
   // Write your code here!
-}
+  if(customerRide < = 400){
+    return('This one is on me!');
+    }
+
+    elseif(customerRide > 2000) {
+      return('I will gladly take your thirty bucks.');
+    }
+
+    elseif(customerRide > 2500) {
+      return('No can do.');
+    }
+  };
+
+
 
 function ternaryCheckCity(){
   // Write your code here!
+  if(customerCity is NYC) {
+    return('Ok, sounds good.');
+  }
 }
+
+  elseif(customerCity != NYC){
+    return('No go.');
+  };
+;
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switchOnCharmFromTip('if the tip is generous') {
+    return('Thank you so much.');
+  };
 }
+
+  switchOnCharmFromTip('if the tip is not as generous') {
+    return('Thank you.');
+  };
+
+  switchOnCharmFromTip('should return "Bye." if anything else'), {
+    return('thanks for everything');
+  };
